cc5f554dfd4f3c1cbdffd5fd50f7083903347c12
Sindy Tatiana Suarez Baron <sitysb@gmail.com>
2025-07-23 15:25:12 -0500

    archivos

--- Main.js
+++ Main.js
@@ -2,40 +2,36 @@ const letras = {
   a: { palabra: "águila", imagen: "https://payload.cargocollective.com/1/6/194016/12196792/1_AGUILA_RGB_1000.jpg", width: 250, height: 200 },
   b: { palabra: "ballena", imagen: "https://i.pinimg.com/originals/24/29/72/242972fa41149ba5632fae85eb6c12d7.jpg", width: 300, height: 200 },
   c: { palabra: "cocodrilo", imagen: "https://tse3.mm.bing.net/th/id/OIP.ciZFxZr_vknf3laUo6IlDwAAAA?rs=1&pid=ImgDetMain&o=7&rm=3", width: 200, height: 200 },
-  d: { palabra: "dado", imagen: "https://cdn.pixabay.com/photo/2012/04/18/13/22/dice-37047_1280.png", width: 180, height: 180 },
-  e: { palabra: "elefante", imagen: "https://cdn.pixabay.com/photo/2012/04/01/16/49/elephant-23612_1280.png", width: 300, height: 200 },
-  f: { palabra: "foca", imagen: "https://cdn.pixabay.com/photo/2021/12/11/13/24/seal-6862100_1280.png", width: 220, height: 160 },
-  g: { palabra: "gato", imagen: "https://cdn.pixabay.com/photo/2017/11/09/21/41/cat-2934720_1280.jpg", width: 250, height: 200 },
-  h: { palabra: "helado", imagen: "https://cdn.pixabay.com/photo/2017/08/30/09/08/ice-cream-2691674_1280.jpg", width: 200, height: 220 },
-  i: { palabra: "iglesia", imagen: "https://cdn.pixabay.com/photo/2016/01/19/15/05/church-1141215_1280.jpg", width: 220, height: 200 },
-  j: { palabra: "jirafa", imagen: "https://cdn.pixabay.com/photo/2020/01/14/16/25/giraffe-4763503_1280.jpg", width: 200, height: 250 },
-  k: { palabra: "koala", imagen: "https://cdn.pixabay.com/photo/2014/06/03/19/39/koala-361208_1280.jpg", width: 200, height: 200 },
-  l: { palabra: "luna", imagen: "https://cdn.pixabay.com/photo/2012/04/10/23/57/moon-26896_1280.png", width: 180, height: 180 },
-  m: { palabra: "mariposa", imagen: "https://cdn.pixabay.com/photo/2017/03/10/21/16/butterfly-2136008_1280.jpg", width: 250, height: 200 },
-  n: { palabra: "nido", imagen: "https://cdn.pixabay.com/photo/2018/03/18/15/00/nest-3237970_1280.jpg", width: 180, height: 180 },
-  o: { palabra: "oso", imagen: "https://cdn.pixabay.com/photo/2017/07/23/18/07/bear-2535886_1280.jpg", width: 250, height: 200 },
-  p: { palabra: "pelota", imagen: "https://cdn.pixabay.com/photo/2012/04/24/12/31/ball-40433_1280.png", width: 180, height: 180 },
-  q: { palabra: "queso", imagen: "https://cdn.pixabay.com/photo/2014/12/21/23/28/cheese-576546_1280.png", width: 180, height: 180 },
-  r: { palabra: "ratón", imagen: "https://cdn.pixabay.com/photo/2017/01/19/11/51/rat-1992477_1280.jpg", width: 200, height: 180 },
-  s: { palabra: "sol", imagen: "https://cdn.pixabay.com/photo/2012/04/13/00/11/sun-31213_1280.png", width: 180, height: 180 },
-  t: { palabra: "tren", imagen: "https://cdn.pixabay.com/photo/2016/02/25/16/36/train-1220100_1280.png", width: 250, height: 150 },
-  u: { palabra: "uva", imagen: "https://cdn.pixabay.com/photo/2014/10/19/20/59/grapes-494759_1280.jpg", width: 180, height: 180 },
-  v: { palabra: "vaca", imagen: "https://cdn.pixabay.com/photo/2016/04/01/10/10/cow-1299980_1280.png", width: 250, height: 200 },
-  w: { palabra: "wifi", imagen: "https://cdn.pixabay.com/photo/2016/03/31/19/16/wifi-1293545_1280.png", width: 180, height: 180 },
-  x: { palabra: "xilófono", imagen: "https://cdn.pixabay.com/photo/2013/07/12/19/22/xylophone-154732_1280.png", width: 200, height: 180 },
-  y: { palabra: "yoyo", imagen: "https://cdn.pixabay.com/photo/2012/04/12/19/20/yoyo-30665_1280.png", width: 180, height: 180 },
-  z: { palabra: "zorro", imagen: "https://cdn.pixabay.com/photo/2021/01/04/18/01/fox-5887904_1280.jpg", width: 250, height: 200 },
+  d: { palabra: "delfin", imagen: "https://i2.wp.com/www.imageneseducativas.com/wp-content/uploads/2017/03/Ficha-letra-d-delfin_c.gif?ssl=1", width: 180, height: 180 },
+  e: { palabra: "elefante", imagen: "https://tse3.mm.bing.net/th/id/OIP.-Wsb5fe9X4oxZKOgy9d3YAHaJ4?rs=1&pid=ImgDetMain&o=7&rm=3", width: 300, height: 200 },
+  f: { palabra: "foca", imagen: "https://1.bp.blogspot.com/-R-A-oliDNFU/YA79hvdr6NI/AAAAAAAA8IU/RhH7V0OCI9cOsDkcX8ZRl0i8Cth0u8DtgCLcBGAsYHQ/s1040/Diapositiva7.JPG", width: 220, height: 160 },
+  g: { palabra: "gato", imagen: "https://i.pinimg.com/originals/4c/8c/90/4c8c90cc7b19b1bbf49f3f8a6e4317ce.jpg", width: 250, height: 200 },
+  h: { palabra: "hormiga", imagen: "https://tse3.mm.bing.net/th/id/OIP.tdLlKUhJixfI3pLtm9BmUwAAAA?rs=1&pid=ImgDetMain&o=7&rm=3", width: 200, height: 220 },
+  i: { palabra: "iguana", imagen: "https://i.pinimg.com/736x/62/90/a2/6290a2f74f2e8c6c0f2b713d20896ef3--primers-mobiles.jpg", width: 220, height: 200 },
+  j: { palabra: "jirafa", imagen: "https://tse4.mm.bing.net/th/id/OIP.-VOy1dRyMInvuP5dusUkbQHaJ4?rs=1&pid=ImgDetMain&o=7&rm=3", width: 200, height: 250 },
+  k: { palabra: "koala", imagen: "https://tse2.mm.bing.net/th/id/OIP.jBDyUcAfVWyHTWf1qXOfnwHaHa?rs=1&pid=ImgDetMain&o=7&rm=3", width: 200, height: 200 },
+  l: { palabra: "león", imagen: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg4OTe-SzKv6MI-ugRWk2Wr2gMIBUzLIYzy-mvK9a24ySRUFQNr4EB7Xb4V02-MJuiu8md9R4WpAKDNwdjqKg5ynqgP6dq1O5o59X2O4prBxGpG0bhzCBlnWrmsZGe0lETTrUBk_03RJl2E3GOy0vqFb3lmNCEO3oQdZcFVmdIVN_8ziGjvJZZnrUt9/s2200/Leon%20Letra_1.jpg", width: 180, height: 180 },
+  m: { palabra: "mono", imagen: "https://tse2.mm.bing.net/th/id/OIP.rNN90tg7HmnnxRsP7aWaiQHaHa?rs=1&pid=ImgDetMain&o=7&rm=3", width: 250, height: 200 },
+  n: { palabra: "nutria", imagen: "https://tse2.mm.bing.net/th/id/OIP.SbY7OCkC5K2rZ0_6PzOAxgHaHa?rs=1&pid=ImgDetMain&o=7&rm=3", width: 180, height: 180 },
+  ñ: { palabra: "ñandú", imagen: "https://i.pinimg.com/originals/4e/7f/ca/4e7fca00be470632ddc6d4fab2134dce.jpg", width: 180, height: 180 },
+  o: { palabra: "oso", imagen: "https://i.pinimg.com/736x/4c/6f/9c/4c6f9c225e1ddf8e6344c012404fa286.jpg", width: 180, height: 180 },
+  p: { palabra: "panda", imagen: "https://img.freepik.com/vector-gratis/dibujado-mano-alfabeto-letra-p-panda-ilustracion-vector_74440-2951.jpg?w=2000", width: 180, height: 180 },
+  q: { palabra: "queso", imagen: "https://i.pinimg.com/originals/4c/fd/ee/4cfdee6877e2a95bc02c87002b27cc46.jpg", width: 180, height: 180 },
+  r: { palabra: "ratón", imagen: "https://i.pinimg.com/originals/9f/68/4b/9f684b75ceab50b2faa29f4b22e8080a.jpg", width: 200, height: 180 },
+  s: { palabra: "serpiente", imagen: "https://thumbs.dreamstime.com/b/el-parque-zool%C3%B3gico-lindo-de-la-historieta-ilustr%C3%B3-alfabeto-con-los-animales-divertidos-espa%C3%B1ol-s-para-serpiente-118457207.jpg", width: 180, height: 180 },
+  t: { palabra: "tortuga", imagen: "https://tse1.mm.bing.net/th/id/OIP.XsVUxwGemBSz5r-cN0bYxAAAAA?rs=1&pid=ImgDetMain&o=7&rm=3", width: 250, height: 150 },
+  u: { palabra: "unicornio", imagen: "https://th.bing.com/th/id/R.f03c4ced3c7fd69a58e1625f8276995a?rik=r1lh3V4c4DX1OQ&riu=http%3a%2f%2fwww.mundoprimaria.com%2fwp-content%2fuploads%2f2015%2f06%2f1_Lecto-10.jpg&ehk=J01WrdgQESQL637X9i0IQYaLnLmnQrnAH7RzVjr1BLA%3d&risl=&pid=ImgRaw&r=0", width: 180, height: 180 },
+  v: { palabra: "vaca", imagen: "https://i.pinimg.com/originals/e1/32/06/e132064b8397fe6a7cf2b9f707ddc089.jpg", width: 250, height: 200 },
+  w: { palabra: "windsurf", imagen: "https://static.guiainfantil.com/pictures/1907-letra-w-fichas-con-el-abecedario-para-ninos.jpg", width: 180, height: 180 },
+  x: { palabra: "xilófono", imagen: "https://static.guiainfantil.com/pictures/1909-4-letra-x-fichas-con-el-abecedario-para-ninos.jpg", width: 200, height: 180 },
+  y: { palabra: "yogurt", imagen: "https://static.guiainfantil.com/pictures/1910-letra-y-fichas-con-el-abecedario-para-ninos.jpg", width: 180, height: 180 },
+  z: { palabra: "zorro", imagen: "https://i.pinimg.com/originals/58/e4/4e/58e44ea114dbd789333866e658f35fe9.jpg", width: 250, height: 200 },
 };
 
 Object.keys(letras).forEach(letra => {
   const btn = document.getElementById(letra);
   if (btn) {
     btn.addEventListener("click", () => {
-      const sonido = new Audio("sonidos/bubble-pop-06-351337.mp3");
-
-      sonido.play().catch((error) => {
-        console.error("Error al reproducir el sonido:", error);
-      });
 
       const { palabra, imagen, width, height } = letras[letra];
 
